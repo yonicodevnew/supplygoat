c599ca315a1e1837ff17b7973bbe2ddc2b60f428
yonicodevnew <103991408+yonicodevnew@users.noreply.github.com>
2022-07-17 12:56:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  
+  
   bucket        = "${local.resource_prefix.value}-data"
   region        = "us-west-2"
   acl           = "public-read"
